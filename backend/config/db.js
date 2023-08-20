b4fe3d78fe02eb72ddfb14e01786e804ec26c70d
jyoti pal <76580894+Jyotipal29@users.noreply.github.com>
2023-08-20 13:17:12 +0530

    sad

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    await mongoose.set("strictQuery", false);
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`mongo db connected:${conn.connection.host}`);
   } catch (error) {
