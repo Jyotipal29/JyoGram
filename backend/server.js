4d9dd2675ace15f4cad39532062cc0f1dd9eaa5b
jyoti pal <76580894+Jyotipal29@users.noreply.github.com>
2023-08-20 13:55:41 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
 });
 
 app.post('/token', (req, res)=>{
-  if(req.header('MY-API-KEY') && req.header('MY-API-KEY') === "3437534sdfasfSDF"){
+  if(req.headers('MY-API-KEY') && req.headers('MY-API-KEY') === "3437534sdfasfSDF"){
     const token = Math.random(999999999999);
     res.status(200).send(token);
   } else {
